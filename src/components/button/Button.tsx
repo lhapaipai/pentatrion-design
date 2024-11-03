796fbbe874039508fd9948988cacee834e3bf7c4
Hugues Tavernier <hugues.tavernier@protonmail.com>
2024-11-03 20:02:03 +0100

    button add active translation

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -54,65 +54,59 @@ export const buttonVariants = {
   variant: {
     contained(color: ThemeColor) {
       return clsx(
-        "shadow hover:shadow-md focus:shadow-md active-full:shadow-lg outline-offset-0",
+        "shadow hover:shadow-md focus:shadow-md active-full:shadow-md-active outline-offset-0",
         {
-          yellow:
-            "text-yellow-text bg-yellow-3 hover:bg-yellow-4 active-full:bg-yellow-5 focus-visible:outline-yellow-5",
-          gray: "text-gray-text bg-gray-3 hover:bg-gray-4 active-full:bg-gray-5 focus-visible:outline-gray-5",
-          red: "text-red-text bg-red-3 hover:bg-red-4 active-full:bg-red-5 focus-visible:outline-red-5",
-          orange:
-            "text-orange-text bg-orange-3 hover:bg-orange-4 active-full:bg-orange-5 focus-visible:outline-orange-5",
-          green:
-            "text-green-text bg-green-3 hover:bg-green-4 active-full:bg-green-5 focus-visible:outline-green-5",
-          blue: "text-blue-text bg-blue-3 hover:bg-blue-4 active-full:bg-blue-5 focus-visible:outline-blue-5",
+          yellow: "text-yellow-text bg-yellow-3 hover:bg-yellow-4 focus-visible:outline-yellow-5",
+          gray: "text-gray-text bg-gray-3 hover:bg-gray-4 focus-visible:outline-gray-5",
+          red: "text-red-text bg-red-3 hover:bg-red-4 focus-visible:outline-red-5",
+          orange: "text-orange-text bg-orange-3 hover:bg-orange-4 focus-visible:outline-orange-5",
+          green: "text-green-text bg-green-3 hover:bg-green-4 focus-visible:outline-green-5",
+          blue: "text-blue-text bg-blue-3 hover:bg-blue-4 focus-visible:outline-blue-5",
         }[color],
       );
     },
     light(color: ThemeColor) {
       return clsx(
-        "shadow hover:shadow-md focus:shadow-md active-full:shadow-lg outline-offset-0 bg-gray-0",
+        "shadow hover:shadow-md focus:shadow-md active-full:shadow-md-active outline-offset-0 bg-gray-0",
         {
           yellow:
-            "text-gray-text hover:text-yellow-text hover:bg-yellow-3 active-full:bg-yellow-4 dark:active-full:bg-yellow-4 focus-visible:outline-yellow-4",
-          gray: "text-gray-text hover:text-gray-text hover:bg-gray-3 active-full:bg-gray-4 dark:active-full:bg-gray-4/50 focus-visible:outline-gray-4",
-          red: "text-gray-text hover:text-red-text hover:bg-red-3 active-full:bg-red-4 dark:active-full:bg-red-4/50 focus-visible:outline-red-4",
+            "text-gray-text hover:text-yellow-text hover:bg-yellow-3 focus-visible:outline-yellow-4",
+          gray: "text-gray-text hover:text-gray-text hover:bg-gray-3 focus-visible:outline-gray-4",
+          red: "text-gray-text hover:text-red-text hover:bg-red-3 focus-visible:outline-red-4",
           orange:
-            "text-gray-text hover:text-orange-text hover:bg-orange-3 active-full:bg-orange-4 dark:active-full:bg-orange-4/50 focus-visible:outline-orange-4",
+            "text-gray-text hover:text-orange-text hover:bg-orange-3 focus-visible:outline-orange-4",
           green:
-            "text-gray-text hover:text-green-text hover:bg-green-3 active-full:bg-green-4 dark:active-full:bg-green-4/50 focus-visible:outline-green-4",
-          blue: "text-gray-text hover:text-blue-text hover:bg-blue-3 active-full:bg-blue-4 dark:active-full:bg-blue-4/50 focus-visible:outline-blue-4",
+            "text-gray-text hover:text-green-text hover:bg-green-3 focus-visible:outline-green-4",
+          blue: "text-gray-text hover:text-blue-text hover:bg-blue-3 focus-visible:outline-blue-4",
         }[color],
       );
     },
     outlined(color: ThemeColor) {
       return clsx(
-        "bg-gray-0 text-gray-7 outline outline-2 outline-offset-[-2px] focus-visible:ring-2 active-full:shadow-md",
+        "bg-gray-0 hover:shadow-sm active-full:shadow-sm-active text-gray-7 outline outline-2 outline-offset-[-2px] focus-visible:ring-2",
         {
-          yellow:
-            "hover:bg-yellow-1/50 active-full:bg-yellow-2/50 outline-yellow-5 focus-visible:ring-yellow-4",
-          gray: "hover:bg-gray-1 active-full:bg-gray-2 outline-gray-5 focus-visible:ring-gray-4",
-          red: "hover:bg-red-1/50 active-full:bg-red-2/50 outline-red-5 focus-visible:ring-red-4",
-          orange:
-            "hover:bg-orange-1/50 active-full:bg-orange-2/50 outline-orange-5 focus-visible:ring-orange-4",
-          green:
-            "hover:bg-green-1/50 active-full:bg-green-2/50 outline-green-5 focus-visible:ring-green-4",
-          blue: "hover:bg-blue-1/50 active-full:bg-blue-2/50 outline-blue-5 focus-visible:ring-blue-4",
+          yellow: "hover:bg-yellow-1/50 outline-yellow-5 focus-visible:ring-yellow-4",
+          gray: "hover:bg-gray-1 outline-gray-5 focus-visible:ring-gray-4",
+          red: "hover:bg-red-1/50 outline-red-5 focus-visible:ring-red-4",
+          orange: "hover:bg-orange-1/50 outline-orange-5 focus-visible:ring-orange-4",
+          green: "hover:bg-green-1/50 outline-green-5 focus-visible:ring-green-4",
+          blue: "hover:bg-blue-1/50 outline-blue-5 focus-visible:ring-blue-4",
         }[color],
       );
     },
     text(color: ThemeColor) {
       return clsx(
-        "bg-transparent active-full:shadow-sm outline-offset-0",
+        "bg-transparent hover:shadow-sm active-full:shadow-sm-active outline-offset-0",
         {
           yellow:
-            "hover:bg-yellow-1/50 active-full:bg-yellow-2/25 text-yellow-4 hover:text-yellow-5 focus-visible:outline-yellow-5",
-          gray: "hover:bg-gray-1 active-full:bg-gray-2 text-gray-5 hover:text-gray-6 active-full:text-gray-7 focus-visible:outline-gray-5",
-          red: "hover:bg-red-1/50 active-full:bg-red-2/50 text-red-4 hover:text-red-5 focus-visible:outline-red-5",
+            "hover:bg-yellow-1/50 text-yellow-4 hover:text-yellow-5 focus-visible:outline-yellow-5",
+          gray: "hover:bg-gray-1 text-gray-5 hover:text-gray-6 active-full:text-gray-7 focus-visible:outline-gray-5",
+          red: "hover:bg-red-1/50 text-red-4 hover:text-red-5 focus-visible:outline-red-5",
           orange:
-            "hover:bg-orange-1/50 active-full:bg-orange-2/50 text-orange-4 hover:text-orange-5 focus-visible:outline-orange-5",
+            "hover:bg-orange-1/50 text-orange-4 hover:text-orange-5 focus-visible:outline-orange-5",
           green:
-            "hover:bg-green-1/50 active-full:bg-green-2/50 text-green-4 hover:text-green-5 focus-visible:outline-green-5",
-          blue: "hover:bg-blue-1/50 active-full:bg-blue-2/50 text-blue-4 hover:text-blue-5 focus-visible:outline-blue-5",
+            "hover:bg-green-1/50 text-green-4 hover:text-green-5 focus-visible:outline-green-5",
+          blue: "hover:bg-blue-1/50 text-blue-4 hover:text-blue-5 focus-visible:outline-blue-5",
         }[color],
       );
     },
@@ -169,7 +163,7 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
         role="button"
         ref={inputRef}
         className={clsx(
-          "relative inline-flex cursor-pointer items-center overflow-clip border-0 text-center leading-5 no-underline duration-300 focus-visible:outline focus-visible:outline-2 motion-safe:transition-color-shadow",
+          "relative inline-flex cursor-pointer items-center overflow-clip border-0 text-center leading-5 no-underline duration-300 focus-visible:outline focus-visible:outline-2 active:translate-y-[1px] motion-safe:transition-color-shadow active:motion-safe:transition-colors",
           icon ? "rounded-full" : "rounded-2xl",
           className,
           buttonVariants.size(icon, size),
--- src/tailwind/pentatrionTw.ts
+++ src/tailwind/pentatrionTw.ts
@@ -162,8 +162,10 @@ export const pentatrionTw = plugin.withOptions(
         },
         boxShadow: {
           sm: "0 1px 2px 0 rgb(0 0 0 / 0.1)",
+          "sm-active": "0 0px 2px 0 rgb(0 0 0 / 0.1)",
           DEFAULT: "0 1px 3px 0 rgb(0 0 0 / 0.15), 0 1px 2px -1px rgb(0 0 0 / 0.15)",
           md: "0 4px 6px -1px rgb(0 0 0 / 0.15), 0 2px 4px -2px rgb(0 0 0 / 0.15)",
+          "md-active": "0 3px 6px -1px rgb(0 0 0 / 0.15), 0 1px 4px -2px rgb(0 0 0 / 0.15)",
           lg: "0 10px 15px -3px rgb(0 0 0 / 0.15), 0 4px 6px -2px rgb(0 0 0 / 0.15)",
           xl: "0 20px 25px -5px rgb(0 0 0 / 0.15), 0 8px 10px -6px rgb(0 0 0 / 0.15)",
           "2xl": "0 25px 50px -12px rgb(0 0 0 / 0.35)",
