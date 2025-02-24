2bdd50a89d52f30cc8cf31e0559a767a1eb57f7f
Hugues Tavernier <hugues.tavernier@protonmail.com>
2025-02-24 14:18:31 +0100

    complete Href styles

--- src/components/href/Href.stories.tsx
+++ src/components/href/Href.stories.tsx
@@ -1,4 +1,4 @@
-import { decoratedLinkVariant, linkVariant } from ".";
+import { decoratedLinkVariant, linkVariant, linkBg, proseLinkVariant } from ".";
 
 const meta = {
   title: "Components/Href",
@@ -7,6 +7,38 @@ export default meta;
 
 export const Context = () => (
   <>
+    <ul>
+      <li></li>
+    </ul>
+    <dl className="mb-8 grid grid-cols-[200px_1fr]">
+      <dt>linkVariant</dt>
+      <dd>
+        <a className={linkVariant} href="#">
+          website
+        </a>
+      </dd>
+
+      <dt>decoratedLinkVariant</dt>
+      <dd>
+        <a className={decoratedLinkVariant} href="#">
+          website
+        </a>
+      </dd>
+
+      <dt>linkBg</dt>
+      <dd>
+        <a className={linkBg} href="#">
+          website
+        </a>
+      </dd>
+
+      <dt>proseLinkVariant</dt>
+      <dd>
+        <a className={proseLinkVariant} href="#">
+          website
+        </a>
+      </dd>
+    </dl>
     <p>
       Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
       been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
@@ -15,9 +47,13 @@ export const Context = () => (
         Visit the Lonlat website
       </a>
       , not only five centuries, but also the leap into electronic typesetting, remaining
-      essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
-      containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
-      PageMaker including versions of Lorem Ipsum.
+      essentially{" "}
+      <a className={linkBg} href="#">
+        unchanged
+      </a>
+      . It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+      passages, and more recently with desktop publishing software like Aldus PageMaker including
+      versions of Lorem Ipsum.
     </p>
     <h2>Href versus Button</h2>
     <p>
@@ -28,9 +64,13 @@ export const Context = () => (
         href component
       </a>
       , not only five centuries, but also the leap into electronic typesetting, remaining
-      essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
-      containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
-      PageMaker including versions of Lorem Ipsum.
+      essentially unchanged. It was{" "}
+      <a className={proseLinkVariant} href="#">
+        popularised
+      </a>{" "}
+      in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
+      recently with desktop publishing software like Aldus PageMaker including versions of Lorem
+      Ipsum.
     </p>
     <h2>With ghost</h2>
     <p>
