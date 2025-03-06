a37c41f7d79c31d4da0f8cfc275de1afe19ff0a6
Hugues Tavernier <hugues.tavernier@protonmail.com>
2025-03-06 13:44:54 +0100

    remove display property for link component

--- src/components/href/index.ts
+++ src/components/href/index.ts
@@ -1,11 +1,11 @@
 export const proseLinkVariant =
-  "cursor-pointer inline-block text-gray-8 underline decoration-gray-8 visited:text-gray-8 hover:decoration-yellow-5 hover:decoration-2 active:text-yellow-5 active:translate-y-[1px]";
+  "cursor-pointer text-gray-8 underline decoration-gray-8 visited:text-gray-8 hover:decoration-yellow-5 hover:decoration-2 active:text-yellow-5 active:translate-y-[1px]";
 
 export const linkVariant =
-  "cursor-pointer inline-block text-gray-6 hover:text-gray-8 active:text-gray-7 active:translate-y-[1px] active:translate-y-[1px]";
+  "cursor-pointer text-gray-6 hover:text-gray-8 active:text-gray-7 active:translate-y-[1px] active:translate-y-[1px]";
 
 export const linkBg =
-  "cursor-pointer relative font-medium inline-block text-gray-8 hover:before:bg-gray-2 hover:before:absolute hover:before:inset-x-[-4px] hover:before:inset-y-[-2px] hover:before:z-[-1] hover:before:rounded-md active:translate-y-[1px]";
+  "cursor-pointer relative font-medium text-gray-8 hover:before:bg-gray-2 hover:before:absolute hover:before:inset-x-[-4px] hover:before:inset-y-[-2px] hover:before:z-[-1] hover:before:rounded-md active:translate-y-[1px]";
 
 export const decoratedLinkVariant =
-  "relative font-bold inline-block z-2 cursor-pointer text-gray-8 hover:underline decoration-gray-8 active:decoration-gray-7 visited:text-gray-8 active:text-gray-7 active:translate-y-[1px]";
+  "relative font-bold z-2 cursor-pointer text-gray-8 hover:underline decoration-gray-8 active:decoration-gray-7 visited:text-gray-8 active:text-gray-7 active:translate-y-[1px]";
