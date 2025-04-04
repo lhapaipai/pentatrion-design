818f79eb101f271a8a3cd51e6318305295510fd2
Hugues Tavernier <hugues.tavernier@protonmail.com>
2025-04-04 14:14:38 +0200

    fix media preview with external media

--- src/components/media-preview/MediaPreview.tsx
+++ src/components/media-preview/MediaPreview.tsx
@@ -37,7 +37,7 @@ export function MediaPreview({
           className={clsx("flex items-center justify-center", squareContainer && "aspect-4/3 p-2")}
         >
           <img
-            src={media.src ?? src}
+            src={src ?? (media.origin === "external" && media.src ? media.src : undefined)}
             width={width ?? media.width}
             height={height ?? media.height}
             className={clsx(
@@ -51,7 +51,7 @@ export function MediaPreview({
       )}
       <div
         className={clsx(
-          "pointer-fine:h-10 pointer-fine:from-gray-7/25 absolute bottom-0 left-0 w-full overflow-hidden rounded-md",
+          "pointer-fine:from-gray-7/25 absolute bottom-0 left-0 w-full overflow-hidden rounded-md pointer-fine:h-10",
           // gradient animation
           "from-gray-7/50 to-gray-7/0 group-hover:from-gray-7/50 bg-linear-to-t transition-[height] duration-150 ease-out",
           "h-full group-hover:h-full",
@@ -59,7 +59,7 @@ export function MediaPreview({
       ></div>
       <div
         className={clsx(
-          "pointer-fine:translate-y-0 pointer-fine:opacity-0 absolute top-1/2 right-0 left-0 flex items-center justify-center transition",
+          "absolute top-1/2 right-0 left-0 flex items-center justify-center transition pointer-fine:translate-y-0 pointer-fine:opacity-0",
           "-translate-y-1/2 group-hover:-translate-y-1/2 group-hover:opacity-100 [:has(:focus)]:-translate-y-1/2 [:has(:focus)]:opacity-100",
         )}
       >
--- src/components/media-preview/types.ts
+++ src/components/media-preview/types.ts
@@ -1,15 +1,21 @@
+type MediaOrigin = "s3Upload" | "localUpload" | "external" | "asset";
+
 export interface Media {
   id: string;
+  origin: MediaOrigin;
+  category: string;
   mimeType: string;
-  src?: string | null;
-  height?: number | null;
   width?: number | null;
+  height?: number | null;
+  src?: string | null;
 }
 
 export interface MediaImage {
   id: string;
+  origin: MediaOrigin;
+  category: string;
   mimeType: string;
-  src?: string | null;
-  height: number;
   width: number;
+  height: number;
+  src?: string | null;
 }
