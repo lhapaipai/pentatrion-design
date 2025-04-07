df144b1978dafb7593c5c960e1e394724d0f4012
Hugues Tavernier <hugues.tavernier@protonmail.com>
2025-04-07 11:17:41 +0200

    fix media preview when not children

--- src/components/media-preview/MediaPreview.tsx
+++ src/components/media-preview/MediaPreview.tsx
@@ -49,22 +49,26 @@ export function MediaPreview({
       ) : (
         <FileIcon media={media} />
       )}
-      <div
-        className={clsx(
-          "pointer-fine:from-gray-7/25 absolute bottom-0 left-0 w-full overflow-hidden rounded-md pointer-fine:h-10",
-          // gradient animation
-          "from-gray-7/50 to-gray-7/0 group-hover:from-gray-7/50 bg-linear-to-t transition-[height] duration-150 ease-out",
-          "h-full group-hover:h-full",
-        )}
-      ></div>
-      <div
-        className={clsx(
-          "absolute top-1/2 right-0 left-0 flex items-center justify-center transition pointer-fine:translate-y-0 pointer-fine:opacity-0",
-          "-translate-y-1/2 group-hover:-translate-y-1/2 group-hover:opacity-100 [:has(:focus)]:-translate-y-1/2 [:has(:focus)]:opacity-100",
-        )}
-      >
-        {children}
-      </div>
+      {children && (
+        <>
+          <div
+            className={clsx(
+              "pointer-fine:from-gray-7/25 absolute bottom-0 left-0 w-full overflow-hidden rounded-md pointer-fine:h-10",
+              // gradient animation
+              "from-gray-7/50 to-gray-7/0 group-hover:from-gray-7/50 bg-linear-to-t transition-[height] duration-150 ease-out",
+              "h-full group-hover:h-full",
+            )}
+          ></div>
+          <div
+            className={clsx(
+              "absolute top-1/2 right-0 left-0 flex items-center justify-center transition pointer-fine:translate-y-0 pointer-fine:opacity-0",
+              "-translate-y-1/2 group-hover:-translate-y-1/2 group-hover:opacity-100 [:has(:focus)]:-translate-y-1/2 [:has(:focus)]:opacity-100",
+            )}
+          >
+            {children}
+          </div>
+        </>
+      )}
     </div>
   );
 }
