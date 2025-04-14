3df813b8edb8619d75d6b366779a37bb410b5e35
Hugues Tavernier <hugues.tavernier@protonmail.com>
2025-04-14 13:40:59 +0200

    href component linkBg enhancement

--- src/components/href/index.ts
+++ src/components/href/index.ts
@@ -5,7 +5,7 @@ export const linkVariant =
   "cursor-pointer text-gray-6 hover:text-gray-8 active:text-gray-7 active:translate-y-[1px] active:translate-y-[1px]";
 
 export const linkBg =
-  "cursor-pointer relative font-medium text-gray-8 hover:before:bg-gray-2 hover:before:absolute hover:before:inset-x-[-4px] hover:before:inset-y-[-2px] hover:before:z-[-1] hover:before:rounded-md active:translate-y-[1px]";
+  "cursor-pointer relative font-medium text-gray-8 hover:before:bg-gray-1/50 hover:before:absolute hover:before:inset-x-[-6px] hover:before:inset-y-[-2px] hover:before:z-[-1] hover:before:rounded-full hover:before:border hover:before:border-gray-2 active:translate-y-[1px]";
 
 export const decoratedLinkVariant =
-  "relative font-bold z-2 cursor-pointer text-gray-8 hover:underline decoration-gray-8 active:decoration-gray-7 visited:text-gray-8 active:text-gray-7 active:translate-y-[1px]";
+  "relative z-2 cursor-pointer text-gray-8 hover:underline decoration-gray-8 visited:text-gray-8 active:translate-y-[1px] inline-block";
