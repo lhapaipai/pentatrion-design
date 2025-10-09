82f71bf49a2cbfc093fa5f0ea6cbda8bb9a269d7
Hugues Tavernier <hugues.tavernier@protonmail.com>
2025-10-09 23:28:08 +0200

    remove pdnd examples

--- src/components/draggable/index.ts
+++
@@ -1,6 +0,0 @@
-export { DropIndicator } from "./DropIndicator";
-export { useDraggableItem } from "./useDraggableItem";
-export { useDraggableMonitor } from "./useDraggableMonitor";
-export { useDraggableMonitorIndex } from "./useDraggableMonitorIndex";
-export { dragStateStyles, isItemData, getItemData, triggerPostMoveFlash } from "./util";
-export { draggableItemDataKey, type DraggableState, type DraggableItemData } from "./types";
