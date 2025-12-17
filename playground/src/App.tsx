b2273f6d7fe4a0798e8452638e66656bf149e363
Hugues Tavernier <hugues.tavernier@protonmail.com>
2025-12-17 23:33:32 +0100

    added select

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,11 +1,43 @@
 import { Button } from "~/components/button";
+import { Select } from "~/components/select";
 
+export const options = [
+  { value: "abbeville", label: "Abbeville" },
+  { value: "agde", label: "Agde" },
+  { value: "agen", label: "Agen" },
+  { value: "aixenprovence", label: "Aix-en-Provence" },
+  { value: "ajaccio", label: "Ajaccio" },
+  { value: "albi", label: "Albi" },
+  { value: "alencon", label: "Alençon" },
+  { value: "amiens", label: "Amiens" },
+  { value: "angers", label: "Angers" },
+  { value: "angouleme", label: "Angoulême" },
+  { value: "annonay", label: "Annonay" },
+  { value: "antibes", label: "Antibes" },
+  { value: "arcachon", label: "Arcachon" },
+  { value: "arles", label: "Arles" },
+  { value: "arras", label: "Arras" },
+  { value: "asnieres-sur-seine", label: "Asnières-sur-Seine" },
+  { value: "aubagne", label: "Aubagne" },
+  { value: "aubervilliers", label: "Aubervilliers" },
+  { value: "aulnay-sous-bois", label: "Aulnay-sous-Bois" },
+  { value: "avignon", label: "Avignon" },
+  { value: "avranches", label: "Avranches" },
+  { value: "avoriaz", label: "Avoriaz" },
+  { value: "avray", label: "Avray" },
+];
 function App() {
   return (
-    <>
+    <div className="p-4">
       <h1>Vite + React</h1>
       <Button>Hello</Button>
-    </>
+      <Select
+        searchable={false}
+        defaultValue="aixenprovence"
+        placeholder="Select your town..."
+        options={options}
+      ></Select>
+    </div>
   );
 }
 
